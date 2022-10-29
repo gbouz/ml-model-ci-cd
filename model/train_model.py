d58c5a76c9a79e8052d0c592e073620027158ab6
GBouz <dirxa@hotmail.com>
2022-10-29 15:17:07 +0200

    Added starter ml with implementations

--- model/train_model.py
+++ model/train_model.py
@@ -1,15 +1,19 @@
 # Script to train machine learning model.
 
+from io import StringIO
+
+import dvc.api
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import compute_model_metrics, inference, train_model, save_model
 
-# Add code to load in the data.
-import dvc.api
-df = pd.read_csv(StringIO(dvc.api.read('../data/census_raw.csv')))
+# load in the data from remote storage
+df = pd.read_csv(StringIO(dvc.api.read('../data/census_clean.csv')))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(df, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -26,5 +30,22 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# Train and save the model.
+model = train_model(X_train, y_train)
+save_model(model, model.__class__.__name__)
+
+# save transformers
+save_model(encoder, encoder.__class__.__name__)
+save_model(lb, lb.__class__.__name__)
+
+# evalute and print results
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(f"The precision score for {model.__class__.__name__} is: {precision}")
+print(f"The recall score for {model.__class__.__name__} is: {recall}")
+print(f"The fbeta score for {model.__class__.__name__} is: {fbeta}")
